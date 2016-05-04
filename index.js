e953c65da04025fb5f69175c1d596d93ff02399f
Soreine <soreine.plume@gmail.com>
2016-05-04 18:40:42 +0200

    Rename immutable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('Preloaded library:');
 console.log('lodash');
 console.log('Q');
-console.log('immutable');
+console.log('Immutable');
 console.log('Path');
 console.log('Reflux');
 
@@ -10,6 +10,6 @@ var r = repl.start("node> ");
 
 r.context.lodash = require('lodash');
 r.context.Q = require('q');
-r.context.immutable = require('immutable');
+r.context.Immutable = require('immutable');
 r.context.Path = require('path');
 r.context.Reflux = require('reflux');
