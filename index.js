1302ec48cb8450c2a20dcf18da4b96140b29e917
Soreine <soreine.plume@gmail.com>
2016-08-25 17:22:22 +0200

    Add short alias for Immutable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('Preloaded library:');
 console.log('lodash');
 console.log('Q');
-console.log('Immutable');
+console.log('Immutable (alias Im)');
 console.log('Path');
 console.log('Reflux');
 console.log('Slate');
@@ -12,6 +12,8 @@ var r = repl.start("node> ");
 r.context.lodash = require('lodash');
 r.context.Q = require('q');
 r.context.Immutable = require('immutable');
+r.context.Im = r.context.Immutable;
 r.context.Path = require('path');
 r.context.Reflux = require('reflux');
 r.context.Slate = require('slate');
+
